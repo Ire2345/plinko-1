61ec17145c77e0e4a6c46535b39993222fd7c300
Ire2345 <73399409+Ire2345@users.noreply.github.com>
2020-12-30 20:59:40 +0000

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,36 @@
+class Plinko{
+
+    constructor(x,y,r)
+	{
+		var options={
+			isStatic:true
+			
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var pos=this.body.position;		
+
+			push()
+			translate(pos.x, pos.y);
+			
+			
+			fill("white")
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+   
+    
+    
+    
+    }
